e3d80e3522da6780d098e6cffc8dcdc3abb51129
Michael Seibt <mseibt202@gmail.com>
2017-09-25 22:01:25 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Main javascript file, common to most of the html files
 
 $(document).ready(function() {
-    
+    var docHeight = $(document).height();
+    console.log('docHeight is' + docHeight);
 });
