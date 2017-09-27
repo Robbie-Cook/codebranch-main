5808fec19d466d89d46a3146ceb2e911984c9823
Michael Seibt <mseibt202@gmail.com>
2017-09-27 05:02:43 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
             }, 10000);
     }
     function animateThis(element) {
+        console.log("Animate called");
         $(element).animate({
             opacity: 0,
             width: headSize+15,
