42bbc9798c06564c17b39c5d0d448a3243f07e0c
Michael Seibt <mseibt202@gmail.com>
2017-09-27 02:28:26 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 // Main javascript file, common to most of the html files
 
 $(document).ready(function() {
+    
+    // sets the height for the fader
     var docHeight = $(document).height();
     $('.backgroundFade').css({
         height: docHeight - 60
     });
+    
+    // About section
+    
+    // Make the heads pop;
+    
+    $('.ripple').css({
+        
+    });
+    
 });
