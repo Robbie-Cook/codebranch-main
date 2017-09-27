8c16c72fc71268783a567763ef44227f09c56f6f
Robbie Cook <robbie@codebranch.io>
2017-09-27 18:52:38 +1300

    red ripple

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 // Main javascript file, common to most of the html files
 
 $(document).ready(function() {
-    
+
     // sets the height for the fader
     var docHeight = $(document).height();
     $('.backgroundFade').css({
         height: docHeight - 60
     });
-    
+
     // About section
-    
-    // Make the heads pop;
+
+    // Make the heads ripple animation
     var headSize = 80;
     function ripple() {
             setTimeout(function() {
-                setTimeout(function() { 
-                    animateThis($('#ripple2'));
-                }, 10000);
-                animateThis($('#ripple1'));
+                setTimeout(function() {
+                    rippleThis($('#ripple2'));
+                }, 9000);
+                rippleThis($('#ripple1'));
                 ripple();
-            }, 10000);
+            }, 7000);
     }
-    function animateThis(element) {
+    function rippleThis(element) {
         $(element).animate({
             opacity: 0,
             width: headSize+15,
@@ -36,7 +36,13 @@ $(document).ready(function() {
           });
         });
     }
-    
+
+
+
     // call the main function
     ripple();
+});
+
+$(".head").click(function() {
+    rippleThis($(this).siblings(.redRipple)[0]);
 });
