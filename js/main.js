21d86e9b1dc13c94da0bed2a09b8f8aac4d8e0ff
Michael Seibt <mseibt202@gmail.com>
2017-09-28 04:02:08 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@
 var rippleThis;
 
 $(document).ready(function() {
+    
+    // Animated text with type.js
+    var typed = new Typed('#typed', {
+        strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
+        typeSpeed: 0,
+        backSpeed: 0,
+        smartBackspace: true, // this is a default
+        loop: true
+    });
 
     // Make the sections the same height as the page
     $('.mainContent').css({
