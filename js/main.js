783e7bb0b36763050a85b735200523d4dfe8415d
Michael Seibt <mseibt202@gmail.com>
2017-09-28 05:12:38 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var rippleThis;
 $(document).ready(function() {
     
     // Animated text with type.js
+    setTimeout(function() {}, 10000):
     var typed6 = new Typed('#typed', {
         strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
         cursorChar: '_',
