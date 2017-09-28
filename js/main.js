d95b5252926a4ab20fde8e63cb5f0a372e70323d
Michael Seibt <mseibt202@gmail.com>
2017-09-28 05:33:23 +0000

    Updated 'js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -53,7 +53,8 @@ $(document).ready(function() {
       //setTimeout(function() {
         $('#typed').text('');
         var typed6 = new Typed('#typed', {
-            strings: ['Quick &amp; Cheap', 'Slick &amp; Adept', ''],
+            strings: ['Quick &amp; Cheap', 'Slick &amp; Adept', 'Sharp &amp; Skilful', 'Beautiful &amp; Customisable',
+                        'Wholesome &amp; Delicious', "100% Organic"],
             typeSpeed: 0,
             backSpeed: 0,
             cursorChar: '_',
