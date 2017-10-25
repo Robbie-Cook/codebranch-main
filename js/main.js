bf0dd1c36036e4d9d8b908ad8edd05234022b16b
Robert-Cook <Robert-Cook@github.com>
2017-10-25 16:52:29 +1300

    auto commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var rippleThis;
 //$(window).on("load",function() {
 $(document).ready(function() {
   // when form elements clicked, set them to selected
-  $('input').click{function() {
+  $('input').click(function() {
     console.log("Porque estoy aqui");
     // $(this).addClass('selected');
   });
